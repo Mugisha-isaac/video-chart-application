4a2eecb935e5e29bda77a92350e9c155a351e810
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-06-01 13:11:58 +0200

    added paper

--- client/video-chart/src/components/video-player.jsx
+++ client/video-chart/src/components/video-player.jsx
@@ -31,6 +31,12 @@ const videoPlayer = ()=>{
        <video playsInline muted ref={'myVideo'} autoPlay className={classes.video}/>
       </Grid>
       </Paper>
+      <Paper className={classes.paper}>
+      <Grid item xs={12} md={6}>
+       <Typography variant="h5" gutterBottom>Name</Typography>
+       <video playsInline  ref={'userVideo'} autoPlay className={classes.video}/>
+      </Grid>
+      </Paper>
       </Grid>
     )
 }
