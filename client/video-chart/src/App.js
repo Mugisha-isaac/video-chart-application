e3357e62cc8a8f8ede38a57324b89a08cf098433
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-06-02 11:02:23 +0200

    adding userMediaDevice

--- client/video-chart/src/App.js
+++ client/video-chart/src/App.js
@@ -1,49 +1,50 @@
 import React from 'react';
-import {Typography,AppBar} from '@material-ui/core';
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import VideoPlayer from './components/video-player';
 import Options from './components/options';
 import Notifications from './components/Notifications';
-import {makeStyles} from '@material-ui/core/styles';
+
 
 const useStyles = makeStyles((theme) => ({
-    appBar: {
-      borderRadius: 15,
-      margin: '30px 100px',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '600px',
-      border: '2px solid black',
-  
-      [theme.breakpoints.down('xs')]: {
-        width: '90%',
-      },
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
     },
-    image: {
-      marginLeft: '15px',
-    },
-    wrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      width: '100%',
-    },
-  }));
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}));
   
 
 const App = ()=>{
     const classes = useStyles();
     return(
-        <div position="static" color="inherit" classNames={classes.wrapper}>
-            <AppBar className={classes.appBar}>
-         <Typography variant="2" align="center">video chart applications</Typography>
-         </AppBar>
-         <videoPlayer/>
-         <Options>
-             <Notifications/>
-         </Options>
-        </div>
+      <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Video Chat</Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
     )
 }
 
